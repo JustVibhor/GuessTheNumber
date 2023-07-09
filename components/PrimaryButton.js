ba480216897079ac33dc13fca483cace3ab86d01
JustVibhor <vibhorebhatneriya@gmail.com>
2023-07-09 22:01:54 +0530

    added ripple effect

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,13 +1,36 @@
+import { Pressable } from "react-native";
 import { View, Text, StyleSheet } from "react-native";
 
 function PrimaryButton({ children }) {
   return (
-    <View>
-      <Text>{children}</Text>
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={styles.buttonInnerContainer}
+        android_ripple={{ color: "#5f2744" }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
     </View>
   );
 }
 
 export default PrimaryButton;
 
-const styles = new StyleSheet.create();
+const styles = new StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+  },
+
+  buttonInnerContainer: {
+    backgroundColor: "#892f5e",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+});
