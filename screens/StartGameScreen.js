b7d58ff201932f1469f2654b3eb1fd14f65be427
JustVibhor <vibhorebhatneriya@gmail.com>
2023-07-09 19:17:36 +0530

    making my own button

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,12 @@
+import { TextInput, View } from "react-native";
+
+function StartGameScreen() {
+  return (
+    <View>
+      <textInput />
+      {/* Buttons */}
+    </View>
+  );
+}
+
+export default StartGameScreen;
