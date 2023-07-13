5927a45a3fecd2bac8eb49c8b7894edddd8c3ea0
JustVibhor <vibhorebhatneriya@gmail.com>
2023-07-13 17:14:20 +0530

    gameoverscreen styling

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,13 +1,42 @@
-import { View, Image } from "react-native";
+import { View, Image, StyleSheet } from "react-native";
 import Title from "../components/ui/Title";
 
 function GameOverScreen() {
   return (
-    <View>
+    <View style={styles.rootContainer}>
       <Title>Game Over</Title>
-      <Image source={require("../assets/images/success.png")} />
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={require("../assets/images/success.png")}
+        />
+      </View>
     </View>
   );
 }
 
 export default GameOverScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    padding: 24,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 200,
+    borderWidth: 3,
+    borderColor: "white",
+    overflow: "hidden",
+    margin: 36,
+  },
+
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
